adb8f0321f09132bc741a70aa1019f8736057e3c
KalithNorbert <68389751+KalithNorbert@users.noreply.github.com>
2020-11-16 18:24:38 +0200

    pb
    profileButton

--- app/src/main/java/com/example/projectandroid/MainActivity.kt
+++ app/src/main/java/com/example/projectandroid/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.projectandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btProfile = findViewById<ImageButton>(R.id.img_butt1)
+
+
+
+
+        btProfile.setOnClickListener{
+            startProfile()
+        }
+
+
     }
+
+    private fun startProfile() {
+        val intent = Intent(this, Profile::class.java)
+        startActivity(intent)
+
+    }
+
 }
---
+++ app/src/main/java/com/example/projectandroid/Profile.kt
@@ -0,0 +1,11 @@
+package com.example.projectandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
