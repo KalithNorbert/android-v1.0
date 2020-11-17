089d085dcb1fd51560501c9fe205878b930a1e2b
KalithNorbert <68389751+KalithNorbert@users.noreply.github.com>
2020-11-17 22:37:02 +0200

    ll
    login layouts

--- app/src/main/java/com/example/projectandroid/Profile.kt
+++ app/src/main/java/com/example/projectandroid/Profile.kt
@@ -1,11 +1,37 @@
 package com.example.projectandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+
+        val loginButton = findViewById<Button>(R.id.btn_login)
+        val registerButton = findViewById<Button>(R.id.btn_register)
+
+
+
+
+        loginButton.setOnClickListener (View.OnClickListener
+        {
+            setContentView(R.layout.login)
+        })
+
+        registerButton.setOnClickListener (View.OnClickListener
+        {
+            setContentView(R.layout.registration)
+        })
+
+
     }
-}
+
+
+
+    }
+
