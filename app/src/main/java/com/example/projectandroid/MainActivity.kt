9875529bbc45d8da11491434a78b604839e388c8
KalithNorbert <68389751+KalithNorbert@users.noreply.github.com>
2020-12-08 17:07:50 +0200

    Fragment change
    Work: mainactivity change profile and restaurants fragment

--- app/src/main/java/com/example/projectandroid/MainActivity.kt
+++ app/src/main/java/com/example/projectandroid/MainActivity.kt
@@ -4,12 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        img_butt.setOnClickListener{
+            img_butt.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_bright));
+            img_butt1.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_dark));
+            val firstFragment = Profile()
+            val manager = supportFragmentManager
+            val transaction = manager.beginTransaction()
+            transaction.replace(R.id.fragment,firstFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+        img_butt1.setOnClickListener {
+            img_butt.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_dark));
+            img_butt1.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_bright));
+            val secondFragment = Restaurants()
+            val manager = supportFragmentManager
+            val transaction = manager.beginTransaction()
+            transaction.replace(R.id.fragment,secondFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
 
     }
 
